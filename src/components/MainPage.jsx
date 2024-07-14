979b6dbfbf8b8c9304b16cb2ff95d427bcb72a59
timmchi <tnmagg@gmail.com>
2024-07-14 13:48:05 +0400

    icons changed to use react icons

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,13 +2,6 @@ import Hero from "./Hero"
 import Skills from "./Skills"
 import Projects from "./Projects"
 import Contact from "./Contact"
-import Carousel from "./Carousel"
-
-const testImages = [
-    { url: 'https://storage.googleapis.com/petbacker/images/blog/2021/orange-cat-slept-whole-day-gone-viral/fat-cat-sleeping-every-time-4.jpg' },
-    { url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s' },
-    { url: 'https://pbs.twimg.com/profile_images/1534365794074755073/ClZmdMG8_400x400.jpg' }
-]
 
 const MainPage = () => {
     return (
@@ -17,7 +10,6 @@ const MainPage = () => {
             <Skills />
             <Projects />
             <Contact />
-            <Carousel images={testImages} />
         </div>
     )
 }
