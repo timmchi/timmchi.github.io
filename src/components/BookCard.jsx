0274f9c24205051a06be8c2d8fccaa485ef43efa
timmchi <tnmagg@gmail.com>
2024-07-16 16:52:03 +0400

    initial book card styling

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,6 +1,6 @@
 const BookCard = ({ book }) => {
     return (
-        <div>
+        <div className="border border-white border-2 rounded-lg text-sm p-2">
             <img src={book.image} alt={`${book.title} image`} />
             <h4>{book.title}</h4>
             <p>{book.author}</p>
