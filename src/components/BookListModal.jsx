be9f2b372beb449ba9beaa406a677a15c2b059f1
timmchi <tnmagg@gmail.com>
2024-07-16 15:23:29 +0400

    modal closes on click outside

--- src/components/BookListModal.jsx
+++ src/components/BookListModal.jsx
@@ -10,13 +10,13 @@ const BookListModal = ({ buttonText }) => {
     }
 
     return (
-        <div>
+        <div onClick={() => setIsOpen(!isOpen)}>
             <button onClick={handleModalAction} className="block text-white bg-teal-700 hover:bg-teal-900 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-lg px-3 py-1.5 text-center">
                 {buttonText}
             </button>
 
-            <div className={`${!isOpen && 'hidden'} fixed  backdrop-blur-sm overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`} tabIndex="-1">
-                <div className="relative p-4 top-1/4 left-1/4 w-full max-w-3xl max-h-full">
+            <div className={`${!isOpen && 'hidden'} fixed backdrop-blur-sm overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`} tabIndex="-1">
+                <div onClick={e => e.stopPropagation()} className="relative p-4 top-1/4 left-1/4 w-full max-w-3xl max-h-full">
                     <div className="relative bg-teal-900 bg-opacity-95 rounded-lg shadow-lg">
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-slate-400">
                             <h3 className="text-xl font-semibold text-white">Favorite Books</h3>
