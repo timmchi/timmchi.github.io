c70108757b8acad2a88792ccc2bff89fb71350e3
timmchi <tnmagg@gmail.com>
2024-08-06 12:28:51 +0300

    contact form functionality added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,21 +2,23 @@ import ContactForm from "./ContactForm";
 import Header from "./Header";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-
 const Contact = () => {
-    return (
-        <div className="h-[90vh] pt-4">
-            <Header text="Contact Me" />
-            <div className="flex justify-center mb-8 font-bold text-lg text-teal-800 dark:text-stone-200 divide-x divide-teal-900 dark:divide-stone-200 pr-4">
-                <p className="pr-2">timurkapusto@gmail.com</p>
-                <a href="https://t.me/tim_mchi" target="_blank" className="flex gap-1.5 pl-2">
-                Telegram
-                <FaExternalLinkAlt className="text-xl mt-0.5"/>
-                </a>
-            </div>
-            <ContactForm />
-        </div>
-    )
-}
+  return (
+    <div className="h-[90vh] pt-4">
+      <Header text="Contact Me" />
+      <div className="flex justify-center mb-8 font-bold text-lg text-teal-800 dark:text-stone-200 divide-teal-900 dark:divide-stone-200 pr-4">
+        <a
+          href="https://t.me/tim_mchi"
+          target="_blank"
+          className="flex gap-1.5 pl-2"
+        >
+          Telegram
+          <FaExternalLinkAlt className="text-xl mt-0.5" />
+        </a>
+      </div>
+      <ContactForm />
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,29 +1,85 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const ContactForm = () => {
-    const [email, setEmail] = useState('')
-    const [topic, setTopic] = useState('')
-    const [message, setMessage] = useState('')
+  const [email, setEmail] = useState("");
+  const [topic, setTopic] = useState("");
+  const [message, setMessage] = useState("");
 
-    return (
-        <div>
-            <form className="max-w-sm mx-auto" onSubmit={() => console.log('hi')}>
-                <div className="mb-5">
-                    <label htmlFor="email" className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200">Your email</label>
-                    <input className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500" type="email" id="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="name@example.com"/>
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="topic" className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200">Topic</label>
-                    <input className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500" id="topic" value={topic} onChange={e => setTopic(e.target.value)} placeholder="e.g. Collaboration"/>
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="message" className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200">Message</label>
-                    <textarea id="message" className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500" rows="4" value={message} onChange={e => setMessage(e.target.value)} placeholder="Your message"/>
-                </div>
-                <button className="w-full py-2 border border-gray-600 rounded-lg text-white font-bold bg-teal-600 hover:bg-teal-900 dark:hover:bg-teal-700 " type="submit">Submit</button>
-            </form>
+  const sendEmail = async (e) => {
+    e.preventDefault();
+
+    await fetch("https://fabform.io/f/JVaQdrr", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, topic, message }),
+    });
+
+    setEmail("");
+    setTopic("");
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <form className="max-w-sm mx-auto" onSubmit={sendEmail}>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200"
+          >
+            Your email
+          </label>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="name@example.com"
+          />
         </div>
-    )
-}
+        <div className="mb-5">
+          <label
+            htmlFor="topic"
+            className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200"
+          >
+            Topic
+          </label>
+          <input
+            className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
+            id="topic"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+            placeholder="e.g. Collaboration"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-bold text-teal-900 dark:text-stone-200"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
+            rows="4"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Your message"
+          />
+        </div>
+        <button
+          className="w-full py-2 border border-gray-600 rounded-lg text-white font-bold bg-teal-600 hover:bg-teal-900 dark:hover:bg-teal-700 "
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
