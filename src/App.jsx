ecfa03924a9c7d9e013e78d45ef3fffaec60b986
timmchi <tnmagg@gmail.com>
2024-08-07 12:28:00 +0300

    persistent theme change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-import ProjectPage from './components/ProjectPage'
-import About from './components/About'
-import Projects from './components/Projects'
-import NavBar from './components/Navbar'
-import MainPage from './components/MainPage'
-import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import ProjectPage from "./components/ProjectPage";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import NavBar from "./components/Navbar";
+import MainPage from "./components/MainPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(false);
 
-  const handleDarkModeToggle = () => setIsDark(!isDark)
+  useEffect(() => {
+    const darkTheme = window.localStorage.getItem("isDark");
+    if (darkTheme) setIsDark(darkTheme === "no" ? true : false);
+  }, []);
+
+  const handleDarkModeToggle = () => {
+    setIsDark(!isDark);
+    isDark
+      ? window.localStorage.setItem("isDark", "yes")
+      : window.localStorage.setItem("isDark", "no");
+  };
 
   return (
-    <div className={`${isDark && 'dark'} bg-stone-200 dark:bg-teal-900 w-full max-w-screen`}>
+    <div
+      className={`${
+        isDark && "dark"
+      } bg-stone-200 dark:bg-teal-900 w-full max-w-screen`}
+    >
       <Routes>
         <Route path="/projects/:projectId" element={<ProjectPage />} />
         <Route path="/about" element={<About />} />
@@ -20,9 +34,9 @@ function App() {
         <Route path="/" element={<MainPage />} />
       </Routes>
 
-      <NavBar darkModeToggle={handleDarkModeToggle} isDark={isDark}/>
+      <NavBar darkModeToggle={handleDarkModeToggle} isDark={isDark} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
