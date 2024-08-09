0db247c41ef1035be587e9ae7f340ede9ab90433
timmchi <tnmagg@gmail.com>
2024-08-09 21:26:49 +0300

    book images, book card styling

--- data/favoriteBooks.js
+++ data/favoriteBooks.js
@@ -1,42 +1,43 @@
+import karamazov from "../bookcovers/brotherskaramazov.jpg";
+import demons from "../bookcovers/demons.jpg";
+import jest from "../bookcovers/infinitejest.jpg";
+import resurrection from "../bookcovers/resurrection.jpg";
+import wap from "../bookcovers/warandpeace.jpg";
+
 export const books = [
   {
     id: 1,
     author: "F. M. Dostoyevsky",
     title: "The Brothers Karamazov",
     link: "https://www.goodreads.com/book/show/4934.The_Brothers_Karamazov",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s",
+    image: karamazov,
   },
   {
     id: 2,
     author: "L. N. Tolstoy",
     title: "War and Peace",
     link: "https://www.goodreads.com/book/show/656.War_and_Peace",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s",
+    image: wap,
   },
   {
     id: 3,
     author: "L. N. Tolstoy",
     title: "Resurrection",
     link: "https://www.goodreads.com/book/show/42641.Resurrection",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s",
+    image: resurrection,
   },
   {
     id: 4,
     author: "F. M. Dostoyevsky",
     title: "Demons",
     link: "https://www.goodreads.com/book/show/5695.Demons",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s",
+    image: demons,
   },
   {
     id: 5,
     author: "D. F. Wallace",
     title: "Infinite Jest",
     link: "https://www.goodreads.com/book/show/6759.Infinite_Jest?from_search=true&from_srp=true&qid=5ekQz1SuRF&rank=1",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx-j7TRghl9jpykKcP7QHZPz8dl0dk_Um49w&s",
+    image: jest,
   },
 ];
