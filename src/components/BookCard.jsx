867adfae4c7ea89343c45690d754f719fe97f537
timmchi <tnmagg@gmail.com>
2024-08-10 10:01:30 +0300

    testing book cards

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -3,7 +3,7 @@ const BookCard = ({ book }) => {
     <div className="text-center text-stone-200 border-stone-200 border-2 shadow-lg rounded-lg text-sm p-2 hover:scale-105 hover:cursor-pointer">
       <a href={book.link} target="_blank">
         <img
-          className="mb-2 border-2 border-teal-700 h-72 w-48 object-cover"
+          className="mb-2 border-2 border-teal-700 h-64 w-52 object-cover"
           src={book.image}
           alt={`${book.title} image`}
         />
