32c9ea2486940178468e58e2d6be2b199230a5f9
timmchi <tnmagg@gmail.com>
2024-09-08 16:25:37 +0300

    react hook form and validation for contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,12 +1,47 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { object, string, minLength, email, pipe } from "valibot";
+import { useForm } from "react-hook-form";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+
+const ContactFormSchema = object({
+  email: pipe(
+    string(),
+    minLength(1, "Please enter your email"),
+    email("The email is incorrectly formatted")
+  ),
+  topic: pipe(
+    string(),
+    minLength(1, "Please enter a topic"),
+    minLength(3, "Topic should be 3 symbols or longer")
+  ),
+  message: pipe(
+    string(),
+    minLength(1, "Please enter your message"),
+    minLength(3, "Message should be 3 symbols or longer")
+  ),
+});
 
 const ContactForm = () => {
-  const [email, setEmail] = useState("");
-  const [topic, setTopic] = useState("");
-  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, isSubmitSuccessful },
+  } = useForm({ resolver: valibotResolver(ContactFormSchema) });
 
-  const sendEmail = async (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        email: "",
+        topic: "",
+        message: "",
+      });
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  const sendEmail = async (data) => {
+    // e.preventDefault();
+    const { email, topic, message } = data;
 
     await fetch("https://fabform.io/f/JVaQdrr", {
       method: "POST",
@@ -15,15 +50,11 @@ const ContactForm = () => {
       },
       body: JSON.stringify({ email, topic, message }),
     });
-
-    setEmail("");
-    setTopic("");
-    setMessage("");
   };
 
   return (
     <div>
-      <form className="max-w-sm mx-auto" onSubmit={sendEmail}>
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit(sendEmail)}>
         <div className="mb-5">
           <label
             htmlFor="email"
@@ -35,8 +66,10 @@ const ContactForm = () => {
             className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            // value={email}
+            defaultValue="email@example.com"
+            {...register("email")}
+            // onChange={(e) => setEmail(e.target.value)}
             placeholder="name@example.com"
           />
         </div>
@@ -50,8 +83,10 @@ const ContactForm = () => {
           <input
             className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
             id="topic"
-            value={topic}
-            onChange={(e) => setTopic(e.target.value)}
+            // value={topic}
+            // onChange={(e) => setTopic(e.target.value)}
+            {...register("topic")}
+            defaultValue="topic"
             placeholder="e.g. Collaboration"
           />
         </div>
@@ -66,8 +101,10 @@ const ContactForm = () => {
             id="message"
             className="border text-sm rounded-lg block w-full p-2.5 bg-teal-700 border-teal-900 placeholder-white text-stone-200 focus:ring-blue-500 focus:border-blue-500"
             rows="4"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            // value={message}
+            // onChange={(e) => setMessage(e.target.value)}
+            {...register("message")}
+            defaultValue="message"
             placeholder="Your message"
           />
         </div>
