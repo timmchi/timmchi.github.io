1ccc6953eee07802656918392507b9e7edcf2332
timmchi <tnmagg@gmail.com>
2024-09-08 16:42:13 +0300

    skill card logo update

--- data/skills.js
+++ data/skills.js
@@ -1,10 +1,19 @@
+import react from "../skillsIcons/react.png";
+import js from "../skillsIcons/js.png";
+import node from "../skillsIcons/node.png";
+import python from "../skillsIcons/Python.png";
+import playwright from "../skillsIcons/playwright.png";
+import cypress from "../skillsIcons/cypress.jpg";
+import css from "../skillsIcons/css.png";
+import html from "../skillsIcons/html.png";
+
 export const skills = [
-  { id: 1, name: "React" },
-  { id: 2, name: "Javascript" },
-  { id: 3, name: "Node" },
-  { id: 4, name: "Python" },
-  { id: 5, name: "Playwright" },
-  { id: 6, name: "Cypress" },
-  { id: 7, name: "CSS" },
-  { id: 8, name: "HTML" },
+  { id: 1, name: "React", image: react },
+  { id: 2, name: "Javascript", image: js },
+  { id: 3, name: "Node", image: node },
+  { id: 4, name: "Python", image: python },
+  { id: 5, name: "Playwright", image: playwright },
+  { id: 6, name: "Cypress", image: cypress },
+  { id: 7, name: "CSS", image: css },
+  { id: 8, name: "HTML", image: html },
 ];
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,10 +1,9 @@
-const IMAGE_PLACEHOLDER =
-  "https://pbs.twimg.com/profile_images/1534365794074755073/ClZmdMG8_400x400.jpg";
-
 const SkillCard = ({ skill }) => {
   return (
     <li className="flex items-center text-stone-200 font-bold bg-teal-700 w-2/3 lg:w-72 mx-4 mb-2 rounded shadow-lg hover:scale-105">
-      <img src={IMAGE_PLACEHOLDER} alt="skill logo" width="70" height="70" />
+      <div className="bg-white">
+        <img src={skill.image} alt="skill logo" width="70" height="70" />
+      </div>
       <p className="text-2xl flex-grow text-center">{skill.name}</p>
     </li>
   );
